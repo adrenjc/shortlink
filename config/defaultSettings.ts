d45e3f81315dfad7e5b77dfc11453f5a0ebc9a60
adrenjc <1145156794@qq.com>
2025-02-13 19:58:59 +0800

    feat: 修改log

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,9 +15,9 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '短链管理',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: 'https://s10.aconvert.com/convert/p3r68-cdx67/aeurt-yt7li.svg',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
