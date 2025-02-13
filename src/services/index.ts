da3b22cebc0dc33a1aaf515ce8075a499d6727c5
adrenjc <1145156794@qq.com>
2025-02-13 15:27:09 +0800

    change some

--- src/services/index.ts
+++ src/services/index.ts
@@ -9,13 +9,13 @@ const ENV_CONFIG = {
     apiUrl: 'http://localhost:8000', // 开发环境
   },
   test: {
-    apiUrl: 'https://test-api.yourdomain.com', // 测试环境
+    apiUrl: 'http://47.107.251.45:3000/api', // 测试环境
   },
   pre: {
     apiUrl: 'http://47.107.251.45:3000/api', // 预发环境
   },
   prod: {
-    apiUrl: 'https://api.yourdomain.com', // 生产环境
+    apiUrl: 'http://47.107.251.45:3000/api', // 生产环境
   },
 };
 
